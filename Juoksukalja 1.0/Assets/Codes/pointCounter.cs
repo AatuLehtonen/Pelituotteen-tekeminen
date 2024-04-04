8b290faff0187065a62193060165f73fcdb7e02e
AatuLehtonen <suomionparas3@gmail.com>
2024-04-04 13:24:41 +0300

    Pointcounter tule näkyviin

--- Juoksukalja 1.0/Assets/Codes/pointCounter.cs
+++ Juoksukalja 1.0/Assets/Codes/pointCounter.cs
@@ -1,22 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 using UnityEngine.UI;
 
 public class pointCounter : MonoBehaviour
 {
     public static pointCounter instance;
 
-    public TMP_Text pointText;
-    public Color color;
+    public Text pointText;
+    public float alphaChangeSpeed = 1f; // Speed at which alpha changes
+    public float targetAlpha = 1f; // Target alpha value (255 in normalized range)
     public int currentPoints = 0;
 
+    private bool isChangingAlpha = false;
+
+    // Start the alpha change process
     public void showCounter()
     {
-        Debug.Log("päästiin tänne");
-        pointText.enableVertexGradient = false;
-        pointText.colorGradient = new VertexGradient(Color.white);
+        isChangingAlpha = true;
     }
 
     void Awake()
@@ -29,6 +30,31 @@ public class pointCounter : MonoBehaviour
         pointText.text = "Points: " + currentPoints.ToString();
     }
 
+    private void Update()
+    {
+        // Check if alpha change process is active
+        if (isChangingAlpha)
+        {
+            // Get current color
+            Color currentColor = pointText.color;
+
+            // Calculate new alpha value
+            float newAlpha = Mathf.MoveTowards(currentColor.a, targetAlpha, alphaChangeSpeed * Time.deltaTime);
+
+            // Update alpha value in the color
+            currentColor.a = newAlpha;
+
+            // Apply the updated color to the text
+            pointText.color = currentColor;
+
+            // Check if the target alpha has been reached
+            if (Mathf.Approximately(newAlpha, targetAlpha))
+            {
+                // Stop the alpha change process
+                isChangingAlpha = false;
+            }
+        }
+    }
     public void IncreasePoints(int v)
     {
         currentPoints += v;
